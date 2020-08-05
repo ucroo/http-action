9004328c17adc21c8c275102466bfbb98bd9ec6b
Igrar Guseynoff <igrar@guseynoff.com>
2020-08-05 20:47:04 +1000

    add axios

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const axios = require('axios');
 
 try {
-  const url  = core.getInput('url');
-  console.log(`URL ${url}!`);
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+    const url  = core.getInput('url');
+    const method = core.getInput('method');
+    const headers = JSON.parse(core.getInput('headers'));
+    const data = JSON.parse(core.getInput('data'));
+
+    axios.request({
+        url: url,
+        method: method,
+        data: JSON.parse(data),
+        headers: JSON.parse(headers)
+    }).then(function(response) {
+        console.log(response.data);
+    });
+    const payload = JSON.stringify(data, undefined, 2)
+    console.log(`The event payload: ${payload}`);
 } catch (error) {
-  core.setFailed(error.message);
+    core.setFailed(error.message);
 }
