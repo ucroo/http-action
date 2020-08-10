eba34a41d5de564622f0a6ccae56455744dbca08
Igrar Guseynoff <igrar@guseynoff.com>
2020-08-10 14:32:17 +1000

    send all payload

--- index.js
+++ index.js
@@ -8,13 +8,6 @@ try {
     const headers = JSON.parse(core.getInput('headers'));
     const data = JSON.parse(core.getInput('data'));
     data["github"] = github.context;
-    data.github.payload.repository = {
-        url: data.github.payload.repository.url
-    };
-    if (github.context.eventName === "pull_request") {
-        data.github.payload.pull_request.head = null;
-        data.github.payload.pull_request.base = null;
-    }
     core.setOutput("response", "processing");
     axios.request({
         url: url,
